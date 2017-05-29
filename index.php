a15780bd029d0b0e2c97dac8d23b8d32319a0ea9
kmchmk1026 <kmchmk@gmail.com>
2017-05-29 19:02:11 +0530

    first push

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!-- saved from url=(0021)https://torrentz2.eu/ -->
+<html lang="en" class="gr__torrentz2_eu">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>ශබ්ද කෝෂය</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style129.css" type="text/css">
+        <link rel="search" type="application/opensearchdescription+xml" href="https://torrentz2.eu/opensearch.xml" title="Torrents Search">
+    </head>
+    <body data-gr-c-s-loaded="true">
+        <div id="wrap">
+            <div id="top">
+                <h1>
+                    <a href="https://torrentz2.eu/" title="Torrents Search">සිංහල<sup>අරුත්</sup></a>
+                </h1>
+                <ul>
+                    <li>
+                        <a href="https://torrentz2.eu/search" title="Torrent Search">සියල්ල</a>
+                    </li>
+<!--                    <li>
+                        <a href="https://torrentz2.eu/my" title="Personal Search">myTorrentz</a>
+                    </li>-->
+                    <li>
+                        <a href="https://torrentz2.eu/help" title="Get Help">උදව්</a>
+                    </li>
+                </ul>
+            </div>
+            <form action="https://torrentz2.eu/search" method="get" class="search" id="search">
+                <fieldset>
+                    <input type="text" name="f" value="" id="thesearchbox" autocomplete="off" autofocus="">
+                    <ul class="autocomplete" style="top: 119px; left: 84.5px; width: 909px;">
+                    </ul>
+                    <input type="submit" id="thesearchbutton" value="සොයන්න">
+                </fieldset>
+            </form>
+            <p class="generic">අරුත් යනු සිංහල වචනවල තේරුම් බලා ගතහැකි ශබ්ද කෝෂයකි.</p>
+            <p class="generic">ඕනෑම සිංහල වචනයක සරල අර්ථය මෙහි විස්තර කර ඇත.</p>
+            <p class="generic">
+                        <a href="http://maduraonline.com/">සිංහල-ඉංග්‍රීසි<img  src="img/cutehamster.gif" title="Hi! search something" alt="">
+                        <!--</a> | <a href="http://torrentzwealmisr.onion/">Torrentz2 Onion<span class="j z spadlock_index"></span>-->
+                        </a> | <a href="http://dictionary.cambridge.org/">ඉංග්‍රීසි-ඉංග්‍රීසි<img  src="img/cutehamster.gif" title="Hi! search something" alt=""></a>
+            </p>
+            <div id="footer">
+                <br>
+            </div>
+            <!--<script type="text/javascript" src="./css/jquery.1.11.2.min.js.download"></script><script type="text/javascript" src="./css/tzs66.js.download"></script>-->
+        </div>
+    </body>
+</html>
